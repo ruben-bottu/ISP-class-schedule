026319dceecabc14b4d9edf3f28ed050aab0e14e
Ruben Bottu <ruben.bottu@student.ucll.be>
2023-10-29 22:59:54 +0100

    Moved secret-load.sql to top level of project because it was the only file left in the db/ folder

--- isp_class_schedule_backend/src/main/resources/init-data/load.sql
+++ isp_class_schedule_backend/src/main/resources/init-data/load.sql
@@ -2,10 +2,9 @@
 This file contains dummy data for example purposes.
 
 How to load my real data?
-    1. create a folder named db in the top level of the project
-    2. within this folder put a file named secret-load.sql
-    3. (optional) convert your iCalendar files into SQL insert statements using the ical_to_sql_insert.py script
-    4. secret-load.sql should contain SQL statements that load data into the init_data table created in setup.sql
+    1. create a file named secret-load.sql at the top level of the project
+    2. (optional) convert your iCalendar files into SQL insert statements using the ical_to_sql_insert.py script
+    3. load your data into the init_data table (created in setup.sql) by putting your SQL inserts in secret-load.sql
 */
 INSERT INTO init_data (start_timestamp, end_timestamp, course_name, group_name)
 VALUES ('2022-06-26 08:00:00', '2022-06-26 10:00:00', 'Algoritmisch denken', 'ME-1TI/1'),
